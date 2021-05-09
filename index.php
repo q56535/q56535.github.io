26e73564c47a10a61404a722e98543f55d4e3f19
q56535 <963750519@qq.com>
2021-05-09 22:21:21 +0800

    Update and rename 123 to index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$arr=file('img.txt');
+$n=count($arr)-1;
+for ($i=1;$i<=1;$i++){
+$x=rand(0,$n);https://github.com/q56535/q56535.github.io/blob/main/123
+header("Location:".$arr[$x],"\n");
+}
+?>
